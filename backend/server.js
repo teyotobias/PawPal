3189aed344d407b7a6538361f8c0041b7fba5be5
teyotobias <teyotobias@utexas.edu>
2024-09-16 10:36:51 -0500

    Simple express app setup.

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+
+// express app
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+});
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${4000}`);
+});
