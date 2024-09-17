e60aceaff019134b7ba24722ecd29d3c8587364b
teyotobias <teyotobias@utexas.edu>
2024-09-16 19:40:31 -0500

    Displaying dogs, form to add one done.

---
+++ frontend/src/components/DogForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+export default function DogForm() {
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [owner, setOwner] = useState("");
+  const [size, setSize] = useState("");
+  const [description, setDescription] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const dog = { name, breed, owner, size, description };
+
+    const response = await fetch("/api/dogs", {
+      method: "POST",
+      body: JSON.stringify(dog),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+    if (response.ok) {
+      setName("");
+      setBreed("");
+      setOwner("");
+      setSize("");
+      setDescription("");
+      setError(null);
+      console.log("new dog added", json);
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Dog</h3>
+      <label>Name:</label>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <label>Breed:</label>
+      <input
+        type="text"
+        onChange={(e) => setBreed(e.target.value)}
+        value={breed}
+      />
+      <label>Owner:</label>
+      <input
+        type="text"
+        onChange={(e) => setOwner(e.target.value)}
+        value={owner}
+      />
+      <label>Size:</label>
+      <select onChange={(e) => setSize(e.target.value)} value={size}>
+        <option value="">Select Size</option> {/* Default placeholder */}
+        <option value="SM">Small (SM)</option>
+        <option value="MD">Medium (MD)</option>
+        <option value="LG">Large (LG)</option>
+      </select>
+      <label>Description:</label>
+      <input
+        type="text"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      />
+      <button>Add Dog</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import DogDetails from "../components/DogDetails";
+import DogForm from "../components/DogForm";
 
 const Home = () => {
   const [dogs, setDogs] = useState(null);
@@ -15,13 +16,14 @@ const Home = () => {
     };
 
     fetchDogs();
-  }, []);
+  }, [dogs]);
 
   return (
     <div className="home">
       <div className="dogs">
         {dogs && dogs.map((dog) => <DogDetails key={dog._id} dog={dog} />)}
       </div>
+      <DogForm />
     </div>
   );
 };
